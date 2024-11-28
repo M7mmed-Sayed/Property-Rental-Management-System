ea2aa7dba86dfb7af68381a0f77ed8108b87df0f
m7mmed-sayed <mohamedsayed1167@gmail.com>
2024-11-28 14:51:11 +0200

    create amenities model and link it with the properties model

--- properties/models/property.py
+++ properties/models/property.py
@@ -1,6 +1,7 @@
 
 from django.db import models
 from user.models import User
+from .amenities import Amenity
 class Property(models.Model):
     description = models.TextField()
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
@@ -11,6 +12,8 @@ class Property(models.Model):
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
     num_rooms = models.IntegerField()
     max_users = models.IntegerField()
+    amenities = models.ManyToManyField(Amenity, blank=True)
+
     def avg_rating(self):
         return self.total_rating / self.num_reviews if self.num_reviews > 0 else 0
     def __str__(self):
