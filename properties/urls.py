b7fabf21f23ff9bd2f7a29b9a8a9745636ef37f3
m7mmed-sayed <mohamedsayed1167@gmail.com>
2024-11-28 10:37:10 +0200

    init properties

---
+++ properties/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import PropertyViewSet,ImagePropertyView
+
+
+urlpatterns = [
+    path('', PropertyViewSet.as_view({'get':'list','post':'create'}),name="properties"),
+    path('edit/<int:pk>/', PropertyViewSet.as_view({'put':'update'}),name="edit-property"),
+    path('delete/<int:pk>/', PropertyViewSet.as_view({'delete':'destroy'}),name="delete-property"),
+    path('edit2/<int:pk>/', PropertyViewSet.as_view({'patch': 'partial_update'}), name="edit2-property"),
+    path('<int:pk>/', PropertyViewSet.as_view({'get': 'retrieve'}), name="property"),
+
+    path('<int:property_id>/images/', ImagePropertyView.as_view({'post':'add'}), name='add-image'),
+    path('<int:property_id>/images/<int:image_id>/', ImagePropertyView.as_view({'delete':'delete'}), name='delete-image'),
+
+]
