fb6127f07c11cc402708fdbf8177c9c6c0fcd95b
m7mmed-sayed <mohamedsayed1167@gmail.com>
2024-11-28 14:53:43 +0200

    cleane the view

--- properties/views/property_view.py
+++ properties/views/property_view.py
@@ -5,14 +5,13 @@ from user.permissions import IsOwnerOrReadOnly
 from django_filters import rest_framework as filters
 from ..serializers import PropertySerializer
 from ..filters import LocationFilter
-from rest_framework.response import Response
 
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = LocationFilter 
-
+    permission_classes=[IsAuthenticated,IsOwnerOrReadOnly]
     # pass the current location to the serialzer to calc the dis for each property
     def get_serializer_context(self):
         context = super().get_serializer_context()
